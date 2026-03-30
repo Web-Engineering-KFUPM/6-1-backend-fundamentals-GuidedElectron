1de45b395911353e7d2f9c5c9e8ba13a7f5c9e90
Loay Shqair <loay.shqair@gmail.com>
2026-03-30 09:10:18 +0300

    sub

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -372,11 +372,13 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
-fetch('http://localhost:3000/student')
-     .then((res) => res.json())
-     .then((data) => {
-       setStudent(data);
-     });
+useEffect(() => {
+  fetch('http://localhost:3000/student')
+    .then((res) => res.json())
+    .then((data) => {
+      setStudent(data);
+    });
+}, []);
 
   return (
     <main className="app-shell">
